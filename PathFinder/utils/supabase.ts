b0e058246fa5dec2269bb9c65824c038aa050328
alfarj83 <148643005+alfarj83@users.noreply.github.com>
2025-11-07 14:43:55 -0500

    added semi-search functionality

--- PathFinder/utils/supabase.ts
+++ PathFinder/utils/supabase.ts
@@ -8,13 +8,6 @@ import AsyncStorage from '@react-native-async-storage/async-storage'
 const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL!;
 const supabaseKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY!;
 
-// --- ADD THESE LINES FOR DEBUGGING ---
-console.log("--- DEBUGGING .env VARIABLES ---");
-console.log("Supabase URL:", supabaseUrl);
-console.log("Supabase Anon Key:", supabaseKey);
-console.log("---------------------------------");
-// ---------------------------------------
-
 if (!supabaseUrl || !supabaseKey) {
     throw new Error("Missing Database URL or ANON key.")
 }
