d7c364e675c3559db8e20e8e539d0c69986a924f
awesomecar1122 <48104518+awesomecar1122@users.noreply.github.com>
2025-11-29 13:05:40 -0500

    Data Tutorial

--- data/temps/sql_insert.sql
+++ data/temps/sql_insert.sql
@@ -1,7 +1,7 @@
 UPDATE professors
-SET rating = 2.4,
-    difficulty = 2.9,
-    num_ratings = 13,
-    faculty_url = 'https://faculty.rpi.edu/james-hendler',
-    rmp_url = 'https://www.ratemyprofessors.com/professor/1352415'
-WHERE full_name = 'James Hendler';
+SET rating = 4.4,
+    difficulty = 4.0,
+    num_ratings = 10,
+    faculty_url = 'https://faculty.rpi.edu/jason-hicken',
+    rmp_url = 'https://www.ratemyprofessors.com/professor/1787926'
+WHERE full_name = 'Jason Hicken';
