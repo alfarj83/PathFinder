62cf17f7efd571ff3543fc78951a3ad0ec0b52da
awesomecar1122 <48104518+awesomecar1122@users.noreply.github.com>
2025-12-04 10:33:36 -0500

    More imgs

--- data/temps/sql_insert.sql
+++ data/temps/sql_insert.sql
@@ -1,7 +1,1 @@
-UPDATE professors
-SET rating = 4.4,
-    difficulty = 4.0,
-    num_ratings = 10,
-    faculty_url = 'https://faculty.rpi.edu/jason-hicken',
-    rmp_url = 'https://www.ratemyprofessors.com/professor/1787926'
-WHERE full_name = 'Jason Hicken';
+https://faculty.rpi.edu/mourad-zeghal
